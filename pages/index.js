dea35bcc8bf2c52b110ff46f7243f07443daf36d
梅木崇浩 <forprogramming.b1@gmail.com>
2022-10-10 13:24:01 +0900

    fix to test client side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 
 const ReadAllItems = (props) => {
+  const [itemData, setItemData] = useState("");
+
+  useEffect(() => {
+    const getAllData = async () => {
+      const response = await fetch("https://nextjs-ref-book-full-stack-app.vercel.app/api/item/readall");
+      // const response = await fetch("http://localhost:4000/api/item/readall");
+      const allItems = await response.json();
+      setItemData(allItems);
+    }
+    getAllData();
+  }, []);
+
   return (
     <div>
       <Head><title>Next Market</title></Head>
       <div className="grid-container-in">
-        {props.allItems.map(item =>
+        {itemData && itemData.allItems.map(item =>
           <Link href={`/item/${item._id}`} key={item._id}>
             <a>
               <Image src={item.image} width="750px" height="500px" alt="item-image" />
@@ -22,15 +35,6 @@ const ReadAllItems = (props) => {
       </div>
     </div>
   );
-};
-
-export const getServerSideProps = async () => {
-  const response = await fetch("https://nextjs-ref-book-full-stack-app.vercel.app/api/item/readall");
-  // const response = await fetch(`http://localhost:4000/api/item/readall`);
-  const allItems = await response.json();
-  return {
-    props: allItems,
-  };
 };
 
 export default ReadAllItems;
