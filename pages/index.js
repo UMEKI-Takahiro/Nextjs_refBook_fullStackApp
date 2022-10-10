457582fca301ba3629e91688538c11e8cb69aec2
梅木崇浩 <forprogramming.b1@gmail.com>
2022-10-10 13:31:30 +0900

    static generation test

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,13 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 
 const ReadAllItems = (props) => {
-  const [itemData, setItemData] = useState("");
-
-  useEffect(() => {
-    const getAllData = async () => {
-      const response = await fetch("https://nextjs-ref-book-full-stack-app.vercel.app/api/item/readall");
-      // const response = await fetch("http://localhost:4000/api/item/readall");
-      const allItems = await response.json();
-      setItemData(allItems);
-    }
-    getAllData();
-  }, []);
-
   return (
     <div>
       <Head><title>Next Market</title></Head>
       <div className="grid-container-in">
-        {itemData && itemData.allItems.map(item =>
+        {props.allItems.map(item =>
           <Link href={`/item/${item._id}`} key={item._id}>
             <a>
               <Image src={item.image} width="750px" height="500px" alt="item-image" />
@@ -38,3 +25,12 @@ const ReadAllItems = (props) => {
 };
 
 export default ReadAllItems;
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://nextjs-ref-book-full-stack-app.vercel.app/api/item/readall");
+  // const response = await fetch("http://localhost:4000/api/item/readall");
+  const allItems = await response.json();
+  return {
+    props: allItems,
+  }
+};
